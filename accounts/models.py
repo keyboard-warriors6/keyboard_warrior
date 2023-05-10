564e89f10ba6738cf37c3d0ea251c75e0a3b697e
Hanui Jang <hany0147@naver.com>
2023-05-10 14:39:27 +0900

    [ADD] user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email field must be set'))
+        if not username:
+            raise ValueError(_('The Username field must be set'))
+        
+        email = self.normalize_email(email)     
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if 'email' in extra_fields:
+            del extra_fields['email']
+
+        return self.create_user(None, username, password, **extra_fields)
+
+
+class User(AbstractUser):
+    def accounts_image_path(instance, filename):
+        return f'accounts/{instance.pk}/{filename}'
+    
+    profile_img = models.ImageField(upload_to=accounts_image_path, null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+    
+
+
