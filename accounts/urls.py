ef6639d5a72a8a8a37e0d77914afb0fbba86bf5d
Hanui Jang <hany0147@naver.com>
2023-05-11 11:05:58 +0900

    [ADD] account delete

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from .views import *
 app_name = 'accounts'
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='singup'),
+    path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('profile/<username>/', ProfileDetailView.as_view(), name='profile'),
-    path('profile/<username>/update', ProfileUpdateView.as_view(), name='update'),
+    path('delete/', UserDeleteView.as_view(), name='delete'),
+    path('profile/<str:username>/', ProfileDetailView.as_view(), name='profile'),
+    path('profile/<str:username>/update/', ProfileUpdateView.as_view(), name='update'),
 
 ]
