f9a969bae27ab6247e325ae6070c73d4a961c128
rloldl <you@example.comii319@naver.comgit config --global user.email you@example.comii319@naver.com>
2023-05-17 09:47:59 +0900

    [ADD] purchase_detail.html

--- static/JS/purchase.js
+++ static/JS/purchase.js
@@ -2,11 +2,25 @@ const infos = document.querySelectorAll('.item-price')
 const totalPrice = document.getElementById('total-price')
 let sum = 0
 
+function AddComma(num){
+  const regexp = /\B(?=(\d{3})+(?!\d))/g;
+  return num.toString().replace(regexp, ',');
+}
+
 infos.forEach((info) => {
-  const price = Number(info.children[0].children[0].textContent)
+  let price = Number(info.children[0].children[0].textContent)
   const cnt = Number(info.children[1].children[0].textContent)
 
   sum += price * cnt
+
+  price = AddComma(price)
+  info.children[0].children[0].textContent = price
 })
 
-totalPrice.textContent = sum
+const total = AddComma(sum);
+totalPrice.textContent = total
+
+const address = document.getElementById('address').textContent
+const addressTag = document.querySelector('[name=address]')
+
+addressTag.value = address
