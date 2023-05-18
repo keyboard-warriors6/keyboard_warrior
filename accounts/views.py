0e4b4186fe1b687d682650922ae9670ae2669059
Hanui Jang <hany0147@naver.com>
2023-05-18 10:49:00 +0900

    [FIX] Profile Purchase

--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from django.db.models import F, Sum
+from django.db.models import F, Sum, Count
 from django.db.models.functions import TruncDate
 from django.views.generic import FormView, RedirectView, DetailView, UpdateView, DeleteView
 from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from products.models import *
+from itertools import groupby
+from operator import itemgetter
 
 
 class SignupView(CreateView):
@@ -38,10 +40,9 @@ class LogoutView(RedirectView):
         logout(request)
         return super().get(request, *args, **kwargs)
     
-
 class ProfileDetailView(LoginRequiredMixin, DetailView):
     model = get_user_model()
-    template_name = 'accounts/profile.html'
+    template_name = 'accounts/profile2.html'
     slug_field = 'username'
     slug_url_kwarg = 'username'
 
@@ -51,18 +52,22 @@ class ProfileDetailView(LoginRequiredMixin, DetailView):
         inquiries = user.inquiry_set.filter(user=self.request.user)
         context['inquiries'] = inquiries
 
-        purchase_list = Purchase.objects.filter(user=self.request.user)
-        purchase_list = purchase_list.order_by('-purchase_date')
-        purchase_list = purchase_list.annotate(date=TruncDate('purchase_date'))
+        # 주문 현황 가져오기
+        purchases = Purchase.objects.filter(user=user).annotate(item_count=Count('products')).order_by('-purchase_date')
 
-        for purchase in purchase_list:
-            purchase_items = PurchaseItem.objects.filter(purchase__user=self.request.user, purchase__purchase_date__date=purchase.date)
-            purchase.purchase_items = purchase_items
+        # 날짜와 시간을 구분하여 묶어주기
+        grouped_purchases = []
+        for purchase in purchases:
+            grouped_purchases.append((purchase.purchase_date.date(), purchase))
 
-        context['purchase_list'] = purchase_list
+        grouped_purchases.sort(key=itemgetter(0), reverse=True)
+        grouped_purchases_by_date = {
+            date: [purchase for _, purchase in group]
+            for date, group in groupby(grouped_purchases, key=itemgetter(0))
+        }
+        context['purchases'] = grouped_purchases_by_date
+
         return context
-    
-    
     
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = get_user_model()
@@ -101,3 +106,31 @@ class UserDeleteView(LoginRequiredMixin, DeleteView):
 
 def permission_denied_view(request, exception):
     return render(request, '403.html', status=403)
+
+
+# class ProfileDetailView(LoginRequiredMixin, DetailView):
+#     model = get_user_model()
+#     template_name = 'accounts/profile2.html'
+#     slug_field = 'username'
+#     slug_url_kwarg = 'username'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         user = self.object
+#         inquiries = user.inquiry_set.filter(user=self.request.user)
+#         context['inquiries'] = inquiries
+
+#         # 주문 현황 가져오기
+#         purchases = Purchase.objects.filter(user=user).annotate(item_count=Count('products')).order_by('-purchase_date')
+#         context['purchases'] = purchases
+
+#         # 날짜별 구매한 상품 가져오기
+#         purchase_items_by_date = {}
+#         for purchase in purchases:
+#             purchase_items = PurchaseItem.objects.filter(purchase=purchase)
+#             purchase_items_by_date[purchase.purchase_date] = purchase_items
+
+#         context['purchase_items_by_date'] = purchase_items_by_date
+
+#         return context
+    
