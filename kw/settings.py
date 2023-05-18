23be4faf03f95c85e83a514a20d362419784aed0
Hanui Jang <hany0147@naver.com>
2023-05-19 03:48:50 +0900

    [FIX] etc5

--- kw/settings.py
+++ kw/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
